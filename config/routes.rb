2e76456a47689638de4d83e87186af3d46af36b2
Alejandro <alejodiaz.123@gmail.com>
2020-07-29 17:20:57 -0400

    Terminado el login con estilos

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  
+  authenticated :user do
+    root 'main#index'
+  end
+
+  unauthenticated :user do
+    root 'main#unauthenticated'
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
